09605adffcf2c1d6920a5a8d69b1246f75753167
ikestar99 <77869090+ikestar99@users.noreply.github.com>
2022-03-14 13:08:45 -0700

    Documented example alignment code

--- flypy/examples/katherinealignment.py
+++ flypy/examples/katherinealignment.py
@@ -16,6 +16,30 @@ from ..pipeline.alignment import alignStack
 
 
 def alignImagesInLIFFile(lifPath, indexes, channel):
+    """
+    Example of complex function to align all images acquired from same z-plane
+    within a .lif file by tileing simpler hyperstack and alignment functions
+
+    @param lifPath: filepath to .lif file that contains images to be aligned
+    @type lifPath: string
+    @param indexes: indexes of images to be aligned within .lif file. Note,
+        .lif files are NOT 0-indexed so the first image in your .lif file has
+        an index of 1. ie, indexes= [5, 6] means that the code will group and
+        align the FIFTH and SIXTH images in your .lif file, regardless of the
+        number that appears next to these jobs in leica or on your imaging
+        sheet. All images to be aligned together MUST have the shape shape
+        (same number of channels, same X/Y dimensions, same z planes) except
+        in the alignment axis, which is the time dimension. This parameter is
+        a list -- if you want to align a single index within your .lif file,
+        input that index as indexes=[index] rather than indexes=index
+    @type indexes: list
+    @param channel: reference channel on which to conduct transformation. Note:
+        unlike the indexes paramterer, leica .lif channels ARE 0-indexed. So,
+        channels=0 indicates an alighnment based on CH0 in leica. To align
+        based on RGECO in ERXXX/RGECO imaging experiments, this parameter
+        should be 1
+    @type channel: int
+    """
     lifFile = loadLifFile(lifPath)
     arrays = [getLifImage(lifFile, idx=idx - 1) for idx in indexes]
     arrays, lengths = concatenateTZCYXArray(arrays, axis=0)
